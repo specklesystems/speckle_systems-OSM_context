1bcb4fcdcd9710aed5ddf3c4c516388065c19e04
KatKatKateryna <kateryna@speckle.systems>
2024-01-11 15:42:26 +0000

    specify version to overlay

--- main.py
+++ main.py
@@ -146,10 +146,10 @@ def automate_function(
         )
 
         # create a commit
-        new_model_id, _ = automate_context.create_new_version_in_project(
+        new_model_id, new_version_id = automate_context.create_new_version_in_project(
             commit_obj, RESULT_BRANCH, "Context from Automate"
         )
-        automate_context.set_context_view([new_model_id], True)
+        automate_context.set_context_view([f"{new_model_id}@{new_version_id}"], True)
 
         if function_inputs.generate_image is True:
             # create and add a basemap png file
